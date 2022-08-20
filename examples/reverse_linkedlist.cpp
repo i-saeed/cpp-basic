79980c2ed2aec6a353b265b45c07e0dd6c9e9f96
i.saeed <ikrima.saeed@agile-robots.com>
2022-08-20 15:40:52 +0200

    build(reverse-LL): CMakeLists updated for reverse linked list example

---
+++ examples/reverse_linkedlist.cpp
@@ -0,0 +1,33 @@
+#include "linkedlist.h"
+
+#include <iostream>
+
+auto main(int argc, char** argv) -> int {
+
+    using namespace linked_list;
+
+    LinkedList<int> l;
+
+    l.push(3);
+    l.push(5);
+    l.push(-60);
+    l.push(90);
+    l.push(-650);
+    l.push(50);
+    l.push(-36);
+    l.push(77);
+    l.push(78);
+
+    l.reverse();
+    // l.reverse();
+
+    std::cout << "\n------------  Done with reversing\n";
+
+    do {
+        std::cout << l.pop() << ", ";
+    } while (!l.empty());
+
+    std::cout << std::endl;
+
+    return 0;
+}
