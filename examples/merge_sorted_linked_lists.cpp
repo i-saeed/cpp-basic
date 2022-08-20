e96772667089a767161b7c3162574edce8d5ae16
i.saeed <ikrima.saeed@agile-robots.com>
2022-08-21 01:29:03 +0200

    chore(merge-ll): WIP example merging ll

--- examples/merge_sorted_linked_lists.cpp
+++ examples/merge_sorted_linked_lists.cpp
@@ -6,36 +6,39 @@ auto main(int argc, char** argv) -> int {
 
     using namespace linked_list;
 
-    LinkedList<int> l1, l2;
-
-    l1.push(-650);
-    l1.push(-60);
-    l1.push(-36);
-    l1.push(3);
-    l1.push(5);
-    l1.push(77);
-    l1.push(90);
-    l1.push(92);
+    LinkedList<int> l1;
     l1.push(100);
+    l1.push(92);
+    l1.push(90);
+    l1.push(77);
+    l1.push(5);
+    l1.push(3);
+    l1.push(-36);
+    l1.push(-60);
+    l1.push(-650);
 
-    l2.push(-650);
-    l2.push(-640);
-    l2.push(-360);
-    l2.push(-37);
-    l2.push(-36);
-    l2.push(-35);
-    l2.push(79);
+    LinkedList<int> l2;
     l2.push(88);
-
-    auto merged_list = mergeTwoLists(l1, l2);
-
-    std::cout << "Size of list:  " << merged_list.size() << std::endl;
-    merged_list.reverse();
-    std::cout << "Size of reversed after reverse:  " << merged_list.size()
-              << std::endl;
+    l2.push(79);
+    l2.push(-35);
+    l2.push(-36);
+    l2.push(-37);
+    l2.push(-360);
+    l2.push(-640);
+    l2.push(-650);
 
     std::cout << "Printing linked list1:  " << l1 << std::endl;
     std::cout << "Printing linked list2:  " << l2 << std::endl;
+
+    auto merged_list = mergeTwoLists(l1, l2);
+    std::cout << "Printing merged_list:  " << merged_list << std::endl;
+
+    l1.clear();
+    l2.clear();
+    std::cout << "Printing linked list1:  " << l1 << std::endl;
+    std::cout << "Printing linked list2:  " << l2 << std::endl;
+
+    merged_list = mergeTwoLists(l1, l2);
     std::cout << "Printing merged_list:  " << merged_list << std::endl;
 
     return 0;
