c8350e6d0a1963573d01bb38dd32b4967c0aee6a
i.saeed <ikrima.saeed@agile-robots.com>
2022-08-22 14:48:19 +0200

    feat(queue): Added queue data structure using 2 stacks

--- src/queue.h
+++ src/queue.h
@@ -1,6 +1,7 @@
 #ifndef _QUEUE_H
 #define _QUEUE_H
 
+#include <optional>
 #include <stack>
 
 namespace queue {
@@ -11,11 +12,13 @@ class QueueViaStacks {
     std::stack<T> top_stack;
     std::stack<T> bottom_stack;
 
+    auto shuffleStacks() -> void;
+
   public:
     auto enqueue(const T& x) -> void;
-    auto dequeue() -> T;
-    auto peek() -> T;
-    auto isEmpty() -> bool;
+    auto dequeue() -> std::optional<T>;
+    auto peek() -> std::optional<T>;
+    auto empty() -> bool;
 };
 
 template <typename T>
@@ -24,13 +27,42 @@ auto QueueViaStacks<T>::enqueue(const T& x) -> void {
 }
 
 template <typename T>
-auto QueueViaStacks<T>::dequeue() -> T {}
+auto QueueViaStacks<T>::dequeue() -> std::optional<T> {
+
+    shuffleStacks();
+    if (bottom_stack.empty())
+        return {};
+
+    auto res = bottom_stack.top();
+    bottom_stack.pop();
+    return res;
+}
 
 template <typename T>
-auto QueueViaStacks<T>::peek() -> T {}
+auto QueueViaStacks<T>::peek() -> std::optional<T> {
+    shuffleStacks();
+
+    if (bottom_stack.empty())
+        return {};
+
+    return bottom_stack.top();
+}
 
 template <typename T>
-auto QueueViaStacks<T>::isEmpty() -> bool {}
+auto QueueViaStacks<T>::empty() -> bool {
+    return (top_stack.empty() && bottom_stack.empty());
+}
+
+template <typename T>
+auto QueueViaStacks<T>::shuffleStacks() -> void {
+
+    if (bottom_stack.empty()) {
+        while (!top_stack.empty()) {
+            bottom_stack.push(top_stack.top());
+            top_stack.pop();
+        }
+    }
+}
 
 } // namespace queue
 #endif
